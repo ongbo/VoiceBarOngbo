eb51dd5de43c22a78ca56c41a13f14f0d88b1754
ongbo <1308161085@qq.com>
2020-03-07 22:39:36 +0800

    kafka and springboot

--- VoiceBarInfoInService/src/main/java/com/voicebar/Control/InfoInControl.java
+++ VoiceBarInfoInService/src/main/java/com/voicebar/Control/InfoInControl.java
@@ -5,17 +5,31 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.voicebar.entity.ResultMessage;
 import com.voicebar.log.*;
+import com.voicebar.utils.ReadProperties;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 @RequestMapping("infolog")
 public class InfoInControl {
+    private final String CollectionDubMaterialTopic = ReadProperties.getKey("CollectionDubMaterial");
+    private final String CollectionDubWorkTopic = ReadProperties.getKey("CollectionDubWork");
+    private final String DubMaterialTopic = ReadProperties.getKey("DubMaterial");
+    private final String LikeDubWorkTopic = ReadProperties.getKey("LikeDubWork");
+    private final String ScanDubMaterialTopic = ReadProperties.getKey("ScanDubMaterial");
+    private final String ScanDubWorkTopic = ReadProperties.getKey("ScanDubWork");
+    private final String TrendDubMaterialTopic = ReadProperties.getKey("TrendDubMaterial");
+    private final String TrendDubWorkTopic = ReadProperties.getKey("TrendDubWork");
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @RequestMapping(value = "helloworld",method = RequestMethod.GET)
     public String helloword(HttpServletRequest req){
@@ -50,27 +64,36 @@ public class InfoInControl {
         if(classname.equals("CollectionDubMaterial")){
             CollectionDubMaterial collectionDubMaterial = JSONObject.parseObject(data, CollectionDubMaterial.class);
             resultmessage = JSONObject.toJSONString(collectionDubMaterial);
+            kafkaTemplate.send(CollectionDubMaterialTopic, resultmessage+"##1##");
         }else if(classname.equals("CollectionDubWork")){
             CollectionDubWork collectionDubWork = JSONObject.parseObject(data, CollectionDubWork.class);
             resultmessage = JSONObject.toJSONString(collectionDubWork);
+            kafkaTemplate.send(CollectionDubWorkTopic, resultmessage+"##1##");
+
         }else if(classname.equals("DubMaterial")){
             DubMaterial dubMaterial = JSONObject.parseObject(data, DubMaterial.class);
             resultmessage = JSONObject.toJSONString(dubMaterial);
+            kafkaTemplate.send(DubMaterialTopic, resultmessage+"##1##");
         }else if(classname.equals("LikeDubWork")){
             LikeDubWork likeDubWork = JSONObject.parseObject(data, LikeDubWork.class);
             resultmessage = JSONObject.toJSONString(likeDubWork);
+            kafkaTemplate.send(LikeDubWorkTopic, resultmessage+"##1##");
         }else if(classname.equals("ScanDubMaterial")){
             ScanDubMaterial scanDubMaterial = JSONObject.parseObject(data, ScanDubMaterial.class);
             resultmessage = JSONObject.toJSONString(scanDubMaterial);
+            kafkaTemplate.send(ScanDubMaterialTopic, resultmessage+"##1##");
         }else if(classname.equals("ScanDubWork")){
             ScanDubWork scanDubWork = JSONObject.parseObject(data, ScanDubWork.class);
             resultmessage = JSONObject.toJSONString(scanDubWork);
+            kafkaTemplate.send(ScanDubWorkTopic, resultmessage+"##1##");
         }else if(classname.equals("TrendDubMaterial")){
             TrendDubMaterial trendDubMaterial = JSONObject.parseObject(data, TrendDubMaterial.class);
             resultmessage = JSONObject.toJSONString(trendDubMaterial);
+            kafkaTemplate.send(TrendDubMaterialTopic, resultmessage+"##1##");
         }else if(classname.equals("TrendDubWork")){
             TrendDubWork trendDubWork = JSONObject.parseObject(data, TrendDubWork.class);
             resultmessage = JSONObject.toJSONString(trendDubWork);
+            kafkaTemplate.send(TrendDubWorkTopic, resultmessage+"##1##");
         }
 
         ResultMessage resultMessage = new ResultMessage();
--- VoiceBarInfoInService/src/main/java/com/voicebar/StartupMain.java
+++ VoiceBarInfoInService/src/main/java/com/voicebar/StartupMain.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 //@EnableAutoConfiguration
+//@EnableAutoConfiguration
 public class StartupMain {
     public static void main(String[] args) {
         SpringApplication.run(StartupMain.class, args);
